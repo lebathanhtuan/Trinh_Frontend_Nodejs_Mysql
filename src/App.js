0f3c51e0db4e2eb2570bdd65769b64e29fefb5e0
trinhdz <trinhlek4@gmail.com>
2023-08-26 10:20:38 +0700

    commit again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Outlet } from "react-router-dom";
 import Admin from "./components/Admin/index.js";
 const App = () => {
   const userInfo = useSelector((state) => state.auth.userInfo);
-  
+
   return (
     <div>
       {userInfo && userInfo.role_id === "1" ? (
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,26 +1,28 @@
-
 import actionTypes from "../actions/actionTypes";
 
+// Lấy dữ liệu từ localStorage (nếu có)
+const storedUserData = localStorage.getItem('userData');
+const initialUserData = storedUserData ? JSON.parse(storedUserData) : null;
+
 const initialState = {
-  isLoggedIn: false,
-  userInfo: null,
+  isLoggedIn: initialUserData ? true : false,
+  userInfo: initialUserData,
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.USER_LOGIN_SUCCESS:
+      // Lưu thông tin đăng nhập vào localStorage
+      localStorage.setItem('userData', JSON.stringify(action.payload));
       return {
         ...state,
         isLoggedIn: true,
         userInfo: action.payload,
       };
     case actionTypes.USER_LOGIN_FAIL:
-      return {
-        ...state,
-        isLoggedIn: false,
-        userInfo: null,
-      };
     case actionTypes.PROCESS_LOGOUT:
+      // Xóa thông tin đăng nhập khỏi localStorage
+      localStorage.removeItem('userData');
       return {
         ...state,
         isLoggedIn: false,
